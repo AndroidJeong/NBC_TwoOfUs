1e9fe42269ecffd73729f5e435ab56578d64bd87
DoDoBest <send1684@gmail.com>
2024-04-22 14:17:33 +0900

    feat: Parcelize를 이용해서 연락처 정보 직렬화 가능하도록 설정
    Co-authored-by: kmtso434 <kmtso434@naver.com>
    Co-authored-by: treeralph <treeralph@gmail.com>
    Co-authored-by:
    neoneoneo123 <gouxrunner@gmail.com>
    Co-authored-by:
    AndroidJeong <okbamtol@gmail.com>

--- app/src/main/java/com/nbc/two_of_us/data/ContactInfo.kt
+++ app/src/main/java/com/nbc/two_of_us/data/ContactInfo.kt
@@ -1,12 +1,15 @@
 package com.nbc.two_of_us.data
 
 import android.net.Uri
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 /**
  * 연락처 정보를 나타내는 클래스 입니다.
  * [이 줄과 아래 내용은 구현 후에 삭제해주세요.]
  * 해당 데이터를 수정해야 하는 경우, var로 변경해서 수정하지 마시고, data class의 copy를 이용해주세요.
  */
+@Parcelize
 data class ContactInfo(
     val rawContactId: Int, // 연락처 정보를 구분하는 고유 값
     val name: String, // 이름
@@ -14,4 +17,4 @@ data class ContactInfo(
     val phone: String, // 전화번호
     val email: String, // 이메일
     val memo: String, // 메모
-)
+): Parcelable

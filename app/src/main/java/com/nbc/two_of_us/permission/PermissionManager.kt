9b3ec260fd39f86fa670e359abc56307c85ed753
treeralph <treeralph@gmail.com>
2024-04-22 18:33:52 +0900

    [FIX] Permission Manager, Lifecycle owner issue

--- app/src/main/java/com/nbc/two_of_us/permission/PermissionManager.kt
+++ app/src/main/java/com/nbc/two_of_us/permission/PermissionManager.kt
@@ -8,8 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
  * 권한 요청을 처리하는 클래스 입니다.
  * 권한 요청을 위해 context 등이 필요한 경우 클래스를 수정해주세요.
  */
-class PermissionManager private constructor(
-    private val activity: AppCompatActivity
+class PermissionManager(
+    private val activity: AppCompatActivity,
 ) {
 
     private var requestPermissionLauncher:  ActivityResultLauncher<String>? = null
@@ -38,15 +38,19 @@ class PermissionManager private constructor(
         }
     }
 
-    companion object {
-        private var INSTANCE : PermissionManager? = null
-        fun getInstance(activity: AppCompatActivity): PermissionManager {
-            return synchronized(PermissionManager::class) {
-                val newInstance = INSTANCE ?: PermissionManager(activity)
-                INSTANCE = newInstance
-                newInstance
-            }
-        }
-    }
+    /**
+     * Lifecycle owner throw Exception
+     * */
+//    companion object {
+//        private var INSTANCE : PermissionManager? = null
+//        fun getInstance(activity: AppCompatActivity): PermissionManager {
+//            return synchronized(PermissionManager::class) {
+//                val newInstance = INSTANCE ?: PermissionManager(activity)
+//                INSTANCE = newInstance
+//                newInstance
+//            }
+//        }
+//    }
+
 }
 

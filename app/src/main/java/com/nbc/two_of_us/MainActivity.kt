183fbe85464219831c1cd5c3f19c9b25c7bedf07
jeong-gyeongwoo <okbamtol@naver.com>
2024-04-22 12:04:29 +0900

    테스트1

--- app/src/main/java/com/nbc/two_of_us/MainActivity.kt
+++ app/src/main/java/com/nbc/two_of_us/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //pr테스트용 코드
+    //pr테스트용 코드11111
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

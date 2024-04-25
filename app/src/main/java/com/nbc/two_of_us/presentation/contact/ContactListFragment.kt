1d7af38101d8391eaf22f6c9a42a5d1a695597dd
JeongGyeongWoo <157380033+AndroidJeong@users.noreply.github.com>
2024-04-25 20:42:59 +0900

    Update ContactListFragment.kt
    replace -> add로 수정

--- app/src/main/java/com/nbc/two_of_us/presentation/contact/ContactListFragment.kt
+++ app/src/main/java/com/nbc/two_of_us/presentation/contact/ContactListFragment.kt
@@ -138,7 +138,7 @@ class ContactListFragment : Fragment() {
                 fragmentDetail.arguments = bundle
 
                 requireActivity().supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container_view, fragmentDetail)
+                    .add(R.id.fragment_container_view, fragmentDetail)
                     .setReorderingAllowed(true)
                     .addToBackStack(null)
                     .commit()
